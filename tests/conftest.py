4ec46213665196448a9be46bca8cb968fa514fa0
Martin Uhrin <martin.uhrin@gmail.com>
2024-10-16 14:33:00 -0300

    Updated tests to use URL if data is not local

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=unused-import, redefined-outer-name
 import pathlib
+from urllib import request
+
 import pytest
 
 from hubbardml import datasets
 from hubbardml import keys
 
+LOCAL_DATA = pathlib.Path(__file__).parent.parent / "data" / "data_uv_2024_1_25.arrow"
+REMOTE_DATA = (
+    "https://archive.materialscloud.org/record/file?record_id=2389&filename=data_uv_2024_1_25.arrow"
+)
+
 
 @pytest.fixture
 def dataframe():  # noqa: F811
-    df = datasets.load(pathlib.Path(__file__).parent.parent / "data" / "data_uv_2022_8_18.json")
+    if not LOCAL_DATA.exists():
+        LOCAL_DATA.parent.mkdir(exist_ok=True)
+        request.urlretrieve(REMOTE_DATA, LOCAL_DATA)
+
+    df = datasets.load(LOCAL_DATA)
+
     # Split into test/train
     datasets.split(df)
 
